e7320c767ed6b2577eb8eb47939def9b1fcd75f3
김도영 <rlaehdud4992@ssu.ac.kr>
2020-01-17 13:09:07 +0900

    initialize my rest-api practice project

---
+++ src/main/java/me/doyoung/practice/demopracticerestapi/DemoPracticeRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.practice.demopracticerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracticeRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPracticeRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/doyoung/practice/demopracticerestapi/DemoPracticeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.practice.demopracticerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracticeRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
