4d25bdb550f9fd0bf53e0731736770edd7fffa69
김도영 <rlaehdud4992@ssu.ac.kr>
2020-01-28 21:06:41 +0900

    @Target, @Rention을 활용하여 TestDescription 애노테이션 구현

--- src/test/java/me/doyoung/practice/demopracticerestapi/EventControllerTest.java
+++ src/test/java/me/doyoung/practice/demopracticerestapi/EventControllerTest.java
@@ -2,6 +2,7 @@ package me.doyoung.practice.demopracticerestapi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.doyoung.practice.demopracticerestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +33,7 @@ public class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -59,6 +61,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_badRequest() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -92,6 +95,7 @@ public class EventControllerTest {
      * @throws Exception
      */
     @Test
+    @TestDescription("입력값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -109,6 +113,7 @@ public class EventControllerTest {
      * @throws Exception
      */
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("spring")
